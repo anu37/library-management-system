14c9cfaf9a27663c8680e3d2c35d35418d8212c6
Anu Abraham <anu@Anus-MacBook-Pro.local>
2025-04-12 22:20:36 +0530

    Adding auth for user login

--- src/main.py
+++ src/main.py
@@ -1,8 +1,14 @@
-from fastapi import FastAPI
-from database import engine, Base
+from fastapi import FastAPI, Depends
+from fastapi.security import OAuth2PasswordBearer
+from typing import Annotated
+from .auth_api import router as auth_router
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
 
 app = FastAPI()
+app.include_router(auth_router)
 
 @app.get("/")
-async def root():
+async def root(token: Annotated[str, Depends(oauth2_scheme)]):
     return {"message": "Hello World"}
